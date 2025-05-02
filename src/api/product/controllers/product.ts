7244573041b5f1f04c4503f627c6d46e8510c7ed
chouksey-shivani <shivani.chouksey@digitalsalt.in>
2025-05-02 19:36:52 +0530

    Customize - product controller and router to implement custom find logic

--- src/api/product/controllers/product.ts
+++ src/api/product/controllers/product.ts
@@ -2,6 +2,24 @@
  * product controller
  */
 
-import { factories } from '@strapi/strapi'
+import {factories} from "@strapi/strapi";
 
-export default factories.createCoreController('api::product.product');
+export default factories.createCoreController("api::product.product", ({strapi}) => ({
+  async find(ctx) {
+    console.log("ctx value inside product", ctx.query);
+    console.log(ctx.query.name);
+    // Safely assert that ctx.query.name is a string, or undefined
+    const name: string | undefined = typeof ctx.query.name === "string" ? ctx.query.name : undefined;
+
+    // Initialize filters only if needed
+    if (name && name?.trim() !== "" && name != undefined) {
+      ctx.query.filters = {
+        name: {$containsi: name},
+      };
+    }
+
+    ctx.query.populate = "*";
+    const data = await super.find(ctx);
+    return data;
+  },
+}));
