fd918a779fe01a2c01f79f25d60ad864a5ceb14b
Shivani-Chouksey <122255940+Shivani-Chouksey@users.noreply.github.com>
2025-05-07 13:51:11 +0530

    Refactor product controller find method to implement dynamic filters for slug, category, and subcategory

--- src/api/product/controllers/product.ts
+++ src/api/product/controllers/product.ts
@@ -7,18 +7,31 @@ import {factories} from "@strapi/strapi";
 export default factories.createCoreController("api::product.product", ({strapi}) => ({
   async find(ctx) {
     console.log("ctx value inside product", ctx.query);
-    console.log(ctx.query.name);
-    // Safely assert that ctx.query.name is a string, or undefined
-    const name: string | undefined = typeof ctx.query.name === "string" ? ctx.query.name : undefined;
+    const { slug, category, subcategory } = ctx.query;
+    const filters: any = {};
+   if(slug && typeof slug === "string"&&slug.trim() !== ""){
+filters.slug={$containsi:slug}
+   }
 
-    // Initialize filters only if needed
-    if (name && name?.trim() !== "" && name != undefined) {
-      ctx.query.filters = {
-        name: {$containsi: name},
-      };
+    if(category && typeof category === "string"&&category.trim() !== ""){
+      filters.categories={
+        categroy_slug:{
+          $eqi: category,
+        }
+      }
     }
-
-    ctx.query.populate = "*";
+if(subcategory && typeof subcategory === "string"&&subcategory.trim() !== ""){
+  filters.sub_categories={
+    subCategory_slug:{
+      $eqi:subcategory,
+    }
+  }
+}
+    ctx.query={
+      ...ctx.query,
+      filters,
+      populate:"*"
+    }
     const data = await super.find(ctx);
     return data;
   },
